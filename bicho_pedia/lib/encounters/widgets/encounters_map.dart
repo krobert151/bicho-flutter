7eccb8dfb6a13bebddcbefc0c85ff9e453161246
Roberto <robertorebolledo151@gmail.com>
2024-03-01 13:29:33 +0100

    to homer

--- bicho_pedia/lib/encounters/widgets/encounters_map.dart
+++ bicho_pedia/lib/encounters/widgets/encounters_map.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:bicho_pedia/encounters/bloc/markes/markers_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -23,19 +25,20 @@ class _MapEncountersState extends State<MapEncounters> {
         builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
           if (snapshot.hasData) {
             return GoogleMap(
-                mapType: MapType.terrain,
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(
-                        snapshot.data!.latitude, snapshot.data!.longitude),
-                    zoom: 14),
-                onMapCreated: (GoogleMapController controller) {
-                  _completer.complete(controller);
-                });
+              mapType: MapType.terrain,
+              initialCameraPosition: CameraPosition(
+                  target:
+                      LatLng(snapshot.data!.latitude, snapshot.data!.longitude),
+                  zoom: 14),
+              onMapCreated: (GoogleMapController controller) {
+                _completer.complete(controller);
+              },
+              markers: _getMarkers(),
+            );
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
 
-          // The connection state is still ongoing
           return Center(child: CircularProgressIndicator());
         },
       ),
@@ -46,12 +49,8 @@ class _MapEncountersState extends State<MapEncounters> {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -59,23 +58,24 @@ class _MapEncountersState extends State<MapEncounters> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
+  }
+
+  _getMarkers() {
+return BlocBuilder<MarkersBloc,MarkersState>(builder: (BuildContext context,MarkersState state) {
+  if(state is MarkersSuccess){
+    return 
+  }
+  return
+},);
+
   }
 }
